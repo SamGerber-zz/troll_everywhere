1cecd6c3c46ba278654ed5beae78ff3b4dade5cc
SamGerber <gerbyxvi@gmail.com>
2016-02-23 15:31:03 -0800

    Adjust relations to accomodate response author relation

--- app/models/response.rb
+++ app/models/response.rb
@@ -4,6 +4,7 @@
 #
 #  id          :integer          not null, primary key
 #  question_id :integer          not null
+#  author_id   :integer          not null
 #  body        :string
 #  image_url   :string
 #  created_at  :datetime         not null
@@ -14,15 +15,17 @@ class Response < ActiveRecord::Base
   validates :question, presence: true
   validates :image_url, url: {no_local: true, allow_blank: true}
 
-  has_many :votes,
+  has_many :votes
 
   belongs_to :question
+
+  belongs_to :author
 
   has_one :poll,
     through: :question,
     source: :poll
 
-  has_one :author,
+  has_one :poll_author,
     through: :poll,
     source: :author
 end
