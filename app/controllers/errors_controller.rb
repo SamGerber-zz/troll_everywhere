246564344f8152cbdbcfc7e99efc8ae228bcee52
SamGerber <gerbyxvi@gmail.com>
2016-02-24 08:48:04 -0800

    Change regex to fix #8
    Replace ^ characters with \A

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,6 @@
 class ErrorsController < ActionController::Base
   def not_found
-    if env["REQUEST_PATH"] =~ /^\/api/ #TODO \A instead of ^ ??
+    if env["REQUEST_PATH"] =~ /\A\/api/
       render :json => {:error => "not-found"}.to_json, :status => 404
     else
       render :text => "404 Not found", :status => 404 #TODO You can render your own template here
@@ -8,7 +8,7 @@ class ErrorsController < ActionController::Base
   end
 
   def exception
-    if env["REQUEST_PATH"] =~ /^\/api/ #TODO \A instead of ^ ??
+    if env["REQUEST_PATH"] =~ /\A\/api/
       render :json => {:error => "internal-server-error"}.to_json, :status => 500
     else
       render :text => "500 Internal Server Error", :status => 500 #TODO You can render your own template here
