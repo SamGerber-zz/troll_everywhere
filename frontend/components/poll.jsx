ba3b3e40b51da632ebe1d76e09145dc9fe517f1d
SamGerber <gerbyxvi@gmail.com>
2016-02-24 17:06:31 -0800

    Write a poll component

--- frontend/components/poll.jsx
+++ frontend/components/poll.jsx
@@ -5,7 +5,31 @@ var Poll = React.createClass({
   render: function() {
 
     return (
-      <p>A Poll!{this.props.poll}</p>
+      <div className="poll-panel-poll group">
+        <ul className="poll-panel-poll-left">
+          <li className="poll-panel-poll-type">💡</li>
+          <li className="poll-panel-poll-checkbox">
+            <input type="checkbox" className="poll-panel-poll-checkbox"/>
+          </li>
+          <li className="poll-panel-poll-name">{this.props.poll}</li>
+        </ul>
+        <ul className="poll-panel-poll-right">
+          <li className="poll-panel-poll-edit">
+            <a className="poll-panel-poll-link" href="#">edit</a>
+          </li>
+          <li className="poll-panel-poll-copy">
+            <a className="poll-panel-poll-link" href="#">copy</a>
+          </li>
+          <li className="poll-panel-poll-share">
+            <a className="poll-panel-poll-link" href="#">share</a>
+          </li>
+          <li className="poll-panel-poll-responses">
+            <a className="poll-panel-poll-link" href="#">8 Responses</a>
+          </li>
+          <li className="poll-panel-poll-name">🔒</li>
+        </ul>
+      </div>
+
     );
   }
 
