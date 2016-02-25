488b9d591ea6dd27cb0a72cac52f89c5af0a60f4
SamGerber <gerbyxvi@gmail.com>
2016-02-25 15:23:59 -0800

    Add router and implement routes for new poll & question

--- frontend/trollEverywhereReact.jsx
+++ frontend/trollEverywhereReact.jsx
@@ -1,1 +1,23 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
+var Link = require('react-router').Link;
+var hashHistory = require('react-router').hashHistory;
+
 var App = require("./components/app");
+var newPoll = require("./components/polls/form.jsx");
+var newQuestion = require("./components/questions/form.jsx");
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  var root = document.getElementById("content");
+  ReactDOM.render((
+    <Router history={hashHistory}>
+      <Route path="/" component={App}></Route>
+      <Route path="/polls/new" component={newPoll}></Route>
+      <Route path="/polls/:id/questions/new/" component={newQuestion}></Route>
+    </Router>
+  ), root);
+});
