b4fc8761a6ebe2e7a45b3f4ebf7e5f8be5037b4b
SamGerber <gerbyxvi@gmail.com>
2016-03-01 04:21:06 -0800

    pushing to heroku

--- frontend/trollEverywhereReact.jsx
+++ frontend/trollEverywhereReact.jsx
@@ -33,8 +33,8 @@ function requireAuth(nextState, replace) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  SessionActions.getCurrentUser(function(){
+SessionActions.getCurrentUser(function(){
+  document.addEventListener("DOMContentLoaded", function() {
     root = document.getElementById("content");
     ReactDOM.render((
       <Router history={browserHistory}>
