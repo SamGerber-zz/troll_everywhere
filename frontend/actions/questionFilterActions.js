f40193069aa88208457150a1d79ea3e800e56124
SamGerber <gerbyxvi@gmail.com>
2016-04-13 16:39:19 -0700

    Reorganize

--- frontend/actions/questionFilterActions.js
+++ frontend/actions/questionFilterActions.js
@@ -1,31 +1,12 @@
 var Dispatcher = require('../dispatcher');
 var QuestionFilterConstants = require('../constants/questionFilterConstants.js');
-var ApiUtil = require('../util/apiUtil.js');
 
 var QuestionFilterActions = {
+
   receiveAllQuestionFilters: function (questionFilters) {
     Dispatcher.dispatch({
       actionType: QuestionFilterConstants.QUESTION_FILTERS_RECEIVED,
       questionFilters: questionFilters
-    });
-  },
-
-  receiveSingleQuestionFilter: function (questionFilter) {
-    Dispatcher.dispatch({
-      actionType: QuestionFilterConstants.QUESTION_FILTER_RECEIVED,
-      questionFilter: questionFilter
-    });
-  },
-
-  createQuestionFilter: function(pollId, questionFilter, callback){
-    var callbacks = [this.receiveSingleQuestionFilter, callback];
-    ApiUtil.createQuestionFilter(pollId, questionFilter, callbacks);
-  },
-
-  updateQuestionFilter: function(questionFilter){
-    Dispatcher.dispatch({
-      actionType: QuestionFilterConstants.UPDATE_QUESTION_FILTER,
-      questionFilter: questionFilter
     });
   },
 
@@ -57,11 +38,13 @@ var QuestionFilterActions = {
       questionIds: questionIds
     });
   },
+
   checkEverything: function(){
     Dispatcher.dispatch({
       actionType: QuestionFilterConstants.CHECK_EVERYTHING,
     });
   },
+  
   checkNothing: function(){
     Dispatcher.dispatch({
       actionType: QuestionFilterConstants.CHECK_NOTHING,
