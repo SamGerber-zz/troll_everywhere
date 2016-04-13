4581d3b383bb6859c998e5cf5cf0b4b0cecc05a3
SamGerber <gerbyxvi@gmail.com>
2016-04-13 16:01:04 -0700

    Reorganized

--- frontend/actions/presentationActions.js
+++ frontend/actions/presentationActions.js
@@ -1,11 +1,9 @@
 var Dispatcher = require('../dispatcher');
 var QuestionConstants = require('../constants/questionConstants.js');
 var ApiUtil = require('../util/apiUtil.js');
-var PollActions = require('./pollActions.js');
-var SessionStore = require('../stores/sessionStore.js');
 
 var PresentationActions = {
-
+  // Read
   receiveSinglePresentation: function (question) {
     Dispatcher.dispatch({
       actionType: QuestionConstants.PRESENTATION_RECEIVED,
