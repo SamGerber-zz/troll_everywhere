04debadbf48b9af2fa4d670be8eeef453cb6b704
SamGerber <gerbyxvi@gmail.com>
2016-04-13 16:00:49 -0700

    Reorganized

--- frontend/actions/pollActions.js
+++ frontend/actions/pollActions.js
@@ -2,13 +2,22 @@ var Dispatcher = require('../dispatcher');
 var PollConstants = require('../constants/pollConstants.js');
 var ApiUtil = require('../util/apiUtil.js');
 
-var ApiActions = {
-  receiveAllPolls: function (polls) {
+var PollActions = {
+  // Create
+  createPoll: function(poll, callback){
+    var callbacks = [this.receiveSinglePoll, callback];
+    ApiUtil.createPoll(poll, callbacks);
+  },
 
-    Dispatcher.dispatch({
-      actionType: PollConstants.POLLS_RECEIVED,
-      polls: polls
-    });
+  // Read
+  fetchSinglePoll: function(PollId, callback){
+    var callbacks = [PollActions.receiveSinglePoll, callback];
+    ApiUtil.fetchSinglePoll(PollId, callbacks);
+  },
+
+  fetchSinglePollForQuestion: function(Question, callback){
+    var callbacks = [PollActions.receiveSinglePoll, callback];
+    ApiUtil.fetchSinglePoll(Question.poll_id, callbacks);
   },
 
   receiveSinglePoll: function (poll) {
@@ -18,35 +27,29 @@ var ApiActions = {
     });
   },
 
-  createPoll: function(poll, callback){
-    var callbacks = [this.receiveSinglePoll, callback];
-    ApiUtil.createPoll(poll, callbacks);
+  fetchAllPolls: function(userId, callback){
+    var callbacks = [PollActions.receiveAllPolls, callback];
+    ApiUtil.fetchAllPolls(userId, callbacks);
   },
 
+  receiveAllPolls: function (polls) {
+    Dispatcher.dispatch({
+      actionType: PollConstants.POLLS_RECEIVED,
+      polls: polls
+    });
+  },
+
+  // Update
   updatePoll: function(poll, callback){
     var callbacks = [this.receiveSinglePoll, callback];
     ApiUtil.updatePoll(poll, callbacks);
   },
 
+  // Destroy
   deletePoll: function(poll, callback){
     var callbacks = [callback];
     ApiUtil.deletePoll(poll, callbacks);
-  },
-
-  fetchAllPolls: function(userId, callback){
-
-    var callbacks = [ApiActions.receiveAllPolls, callback];
-    ApiUtil.fetchAllPolls(userId, callbacks);
-  },
-
-  fetchSinglePoll: function(PollId, callback){
-    var callbacks = [ApiActions.receiveSinglePoll, callback];
-    ApiUtil.fetchSinglePoll(PollId, callbacks);
-  },
-  fetchSinglePollForQuestion: function(Question, callback){
-    var callbacks = [ApiActions.receiveSinglePoll, callback];
-    ApiUtil.fetchSinglePoll(Question.poll_id, callbacks);
   }
 };
 
-module.exports = ApiActions;
+module.exports = PollActions;
